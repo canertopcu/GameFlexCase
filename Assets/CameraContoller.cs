b8b248ae315996123cc42013937056ab66c68e25
CanerTopcu <caner.topcu@hotmail.com>
2022-09-08 14:08:20 +0300

    Responsıve camera added

---
+++ Assets/CameraContoller.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContoller : MonoBehaviour
+{
+    private float _ratio = 0;
+    private float ratio
+    {
+        get => _ratio;
+        set
+        {
+            if (_ratio != value)
+            {
+                _ratio = value;
+                SetHeightByRatio(value);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ratio = (float)Screen.width / (float)Screen.height;
+
+
+
+
+    }
+
+    void SetHeightByRatio(float ratio)
+    {
+        float minFoV = 50;
+        float maxFoV = 127;
+
+        float diffHeight = maxFoV - minFoV;
+
+        float minRatio = 0.165f;
+        float maxRatio = 0.8f;
+        if (ratio > maxRatio)
+        {
+            ratio = maxRatio;
+        }
+        if (ratio < minRatio)
+        {
+            ratio = minRatio;
+        }
+        float normalizedRatio = (ratio - minRatio) / (maxRatio - minRatio);
+        float height = minFoV + diffHeight * Mathf.Pow(1 - normalizedRatio, 3);
+        Camera.main.fieldOfView = height;
+
+    }
+
+}
